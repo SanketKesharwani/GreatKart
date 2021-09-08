c1921371a42ada38de8064a02144e87f26962df0
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-09-08 15:01:49 +0530

    slug field changed

--- category/models.py
+++ category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=200,unique=True)
-    slug = models.CharField(max_length=200,unique=True)
+    slug = models.SlugField(max_length=200,unique=True)
     description = models.TextField(blank=True,max_length=200)
     category_image = models.ImageField(blank=True,upload_to='photos/category_image')
 
