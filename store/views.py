14561a866c02e07b03bcdae93de8aaa84877b87d
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-09-18 14:30:30 +0530

    paginator and in_cart finctionality

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.http import HttpResponse
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 def store(request,category_slug=None):
     categories = None
@@ -11,10 +15,13 @@ def store(request,category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products,6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products':products,
+        'products':paged_products,
         'product_count':product_count,
     }
     return render(request, 'store/store.html',context)
@@ -22,9 +29,11 @@ def store(request,category_slug=None):
 def product_detail(request,category_slug,product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__card_id = _cart_id(request),product=single_product).exists()
     except Exception as e:
         raise e
     context = {
         'single_product':single_product,
+        'in_cart':in_cart,
     }
     return render(request,'store/product_detail.html',context)
