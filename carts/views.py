b248f156d54b7dba44260f3b5a3c6e8568ab51c7
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-09-19 12:42:37 +0530

    minor fix

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from store.models import Product
 from .models import Cart,CartItem
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 def _cart_id(request):
@@ -59,7 +60,7 @@ def cart(request,total=0,quantity=0,cart_items=None):
             quantity += cart_item.quantity
         tax = (2*total)/100
         grand_total = total + tax
-    except DoesNotExist:
+    except ObjectDoesNotExist:
         pass
     context = {
         'total': total,
