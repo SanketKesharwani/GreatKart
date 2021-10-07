78c330a16ee6d83a27bf6f3386b96162b9845bed
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-10-07 12:49:18 +0530

    registration form

---
+++ accounts/forms.py
@@ -0,0 +1,20 @@
+from django import  forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'email', 'password', 'phone']
+
+    def __init__(self,*args,**kwargs):
+        super(RegistrationForm, self).__init__(*args,**kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email'
+        self.fields['phone'].widget.attrs['placeholder'] = 'Enter phone number'
+        self.fields['password'].widget.attrs['placeholder'] = 'Enter Password'
+        self.fields['confirm_password'].widget.attrs['placeholder'] = 'Confirm Password'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
