1def23150e7641e845d00bce50176eb061fdf0ec
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-10-10 13:37:24 +0530

    user email verification and activate views

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,13 @@ from .forms import RegistrationForm
 from .models import Account,MyAccountManager
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
+#ACTIVATION IMPORTS
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes,force_text
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 # Create your views here.
 def register(request):
@@ -18,14 +25,42 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password,username=username)
             user.phone = phone
             user.save()
-            messages.success(request,'Registration Successful')
-            return redirect('register')
+            #USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please Activate Your Account'
+            message = render_to_string('accounts/account_verification_mail.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject,message,to=[to_email])
+            send_email.send()
+            #messages.success(request,'Thankyou for registration Please verify your email to proceed')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
     context = {
         'form':form,
     }
     return render(request,"accounts/register.html",context)
+
+def activate(request,uidb64,token):
+    User = auth.get_user_model()
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request,'Thank you for your email confirmation. Now you can login your account.')
+        return redirect('login')
+    else:
+        messages.error(request,'Activation link is invalid!')
+        return redirect('register')
 
 def login(request):
     if request.method == 'POST':
@@ -45,3 +80,7 @@ def logout(request):
     auth.logout(request)
     messages.success(request,'You are logged out')
     return redirect('login')
+
+@login_required(login_url = 'login')
+def dashboard(request):
+    return render(request,'accounts/dashboard.html')
