8808803383c145e410b2818ed3d7934376e809e0
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-10-11 14:07:26 +0530

    minor change

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -141,6 +141,6 @@ MESSAGE_TAGS = {
 #SMTP Configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '98sanketkesarwani@gmail.com'
-EMAIL_HOST_PASSWORD = '8953173545555'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
