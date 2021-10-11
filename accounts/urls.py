bb9d87c65a82e97db2520eccaf4a8c3b3cabad21
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-10-11 14:06:20 +0530

    forgetpass,resetpass url mapping

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('logout/', views.logout,name='logout'),
     path('activate/<uidb64>/<token>/',views.activate,name='activate'),
     path('dashboard/',views.dashboard,name='dashboard'),
+    path('forgetpassword/',views.forgetpassword,name='forgetpassword'),
+    path('resetpassword/',views.resetpassword,name='resetpassword'),
+    path('reset_password_validate/<uidb64>/<token>/',views.reset_password_validate,name='reset_password_validate'),
 ]
