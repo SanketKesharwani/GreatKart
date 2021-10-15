adbb8c908e9cb96edced2db4bb13b3f7f112d9e8
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-10-15 12:54:23 +0530

    login product grouping

--- accounts/views.py
+++ accounts/views.py
@@ -75,13 +75,38 @@ def login(request):
                 is_cart_item_exist = CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exist:
                     cart_item = CartItem.objects.filter(cart=cart)
+                    #getting product variation from cart id
+                    product_variation = []
                     for item in cart_item:
-                        item.user = user
-                        item.save()
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    #get the cart item from user
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id  = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
             except:
                 pass
             auth.login(request,user)
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request,'Invalid credentials')
             return redirect('login')
