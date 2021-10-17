1644039ae4183df766c5e05af228b4d4115c83d0
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-10-17 13:21:20 +0530

    placeordr views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from carts.models import CartItem
 
 # Create your views here.
+def place_order(request):
+    current_user = request.user
+    cart_item = CartItem.objects.filter(user=current_user)
+    cart_count = cart_item.count()
+    if cart_count <= 0:
+        return redirect('store')
