0297b68b202bc5a8818bc31de39f569947f128c3
Sanket-Code-98 <98sanketkesharwani@gmail.com>
2021-10-23 11:43:44 +0530

    Orderproductinline class

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Payment,Order,OrderProduct
 # Register your models here.
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('payment', 'user', 'product', 'quantity', 'product_price', 'ordered')
+    extra  = 0
+
 admin.site.register(Payment)
 admin.site.register(Order)
 admin.site.register(OrderProduct)
